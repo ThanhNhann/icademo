77401696ada477bf55659dc29b8f3f93102b8693
ThanhNhann <thanhnhan98qh@gmail.com>
2025-05-28 02:57:48 +0700

    update icq

--- x/interchainquery/types/errors.go
+++ x/interchainquery/types/errors.go
@@ -3,5 +3,9 @@ package types
 import "errors"
 
 var (
-	ErrAlreadyFulfilled = errors.New("query already fulfilled")
+	ErrAlreadyFulfilled  = errors.New("query already fulfilled")
+	ErrInvalidICQRequest = errors.New("invalid ICQ request")
+	ErrInvalidICQProof  = errors.New("invalid ICQ proof")
+	ErrFailedToRetryQuery = errors.New("failed to retry query")
+	ErrICQCallbackNotFound = errors.New("ICQ callback not found")
 )
